b12525f405dc5e20acc73dcd998b66bc04b7f8bd
Gerard Dominic Vizcocho <vizcocho.gerarddominic@ue.edu.ph>
2021-07-15 04:39:19 +0800

    Update JobOrder.js

--- models/JobOrder.js
+++ models/JobOrder.js
@@ -1,18 +1,28 @@
 const mongoose = require("mongoose");
 const AutoIncrement = require("mongoose-sequence")(mongoose);
 
+const EquipmentUsed = mongoose.Schema({
+    _id: false,
+    equipmentID: mongoose.Schema.Types.ObjectId,
+    quantity: Number,
+});
+
 const JobOrderSchema = mongoose.Schema({
     prefix: String,
-    description: String,
-    status: String,
-    type: String,
+    date: Date,
+    jobDate: Date,
+    branch: String,
+    status: String, // Can be "PENDING", "ONGOING", "CLOSED"
+    type: String, // Can be "INSTALLATION", "MAINTENANCE"
     remarks: String,
-    equipmentsIDs: [mongoose.Schema.Types.ObjectId],
-    inquiryID: mongoose.Schema.Types.ObjectId,
+    equipmentsUsed: [EquipmentUsed],
+    inquiryID: mongoose.Schema.Types.ObjectId, // For MAINTENANCE JOs only
+    applicationID: mongoose.Schema.Types.ObjectId, // For INSTALLATIONS JOs only
     accountID: mongoose.Schema.Types.ObjectId,
     teamID: mongoose.Schema.Types.ObjectId,
     isDeleted: Boolean,
 });
 
 JobOrderSchema.plugin(AutoIncrement, { inc_field: "jo_ctr" });
+
 module.exports = mongoose.model("JobOrders", JobOrderSchema);
