0892ad779028ce8fbc10279e5d015b7996f67140
Gerard Dominic Vizcocho <vizcocho.gerarddominic@ue.edu.ph>
2021-10-26 21:55:19 +0800

    PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps: [
+        {
+            script: "index.js",
+            watch: ".",
+            instances: "max",
+            exec_mode: "cluster",
+            env: {
+                NODE_ENV: "development",
+            },
+            env_production: {
+                NODE_ENV: "production",
+            },
+        },
+        {
+            script: "./service-worker/",
+            watch: ["./service-worker"],
+        },
+    ],
+};
